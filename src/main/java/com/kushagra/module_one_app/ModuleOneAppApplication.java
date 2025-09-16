384d9f596edf1cf5b92c89db7fc09a6a9b165c38
kushagra <kushagra.kushagrapatel@gmail.com>
2025-09-16 19:18:03 +0530

    Initial Spring Boot project

---
+++ src/main/java/com/kushagra/module_one_app/ModuleOneAppApplication.java
@@ -0,0 +1,13 @@
+package com.kushagra.module_one_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleOneAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuleOneAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kushagra/module_one_app/ModuleOneAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kushagra.module_one_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleOneAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
