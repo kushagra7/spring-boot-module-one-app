00ce978248f3f5d3ec2a4dc496fcaa40599b6431
kushagra <kushagra.kushagrapatel@gmail.com>
2025-09-16 21:19:18 +0530

    added get method

--- src/main/java/com/kushagra/module_one_app/ModuleOneAppApplication.java
+++ src/main/java/com/kushagra/module_one_app/ModuleOneAppApplication.java
@@ -2,12 +2,20 @@ package com.kushagra.module_one_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ModuleOneAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ModuleOneAppApplication.class, args);
+	}
+
+	@GetMapping
+	public String helloWorld(){
+		return "Hello World in Spring Boot";
 	}
 
 }
